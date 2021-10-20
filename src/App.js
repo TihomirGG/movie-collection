fde13480e2f049c752d6d14de74a066e34044721
Tihomir <tihomir.gospodinov@abv.bg>
2021-10-20 13:13:43 +0300

    router works

--- src/App.js
+++ src/App.js
@@ -1,54 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import ROUTES from './constants/routes'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+     <Router>
+       <Switch>
+         <Route exact path={ROUTES.SEARCH}>
+           <div>Hello</div>
+         </Route>
+         <Route exact path={ROUTES.MOVIE}>
+           <div>Hello</div>
+         </Route>
+         <Route exact path={ROUTES.HOME}>
+           <div>Hello</div>
+         </Route>
+       </Switch>
+     </Router>
     </div>
   );
 }
